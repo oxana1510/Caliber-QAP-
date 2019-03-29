d0d42c198ea6f7bc2d22be6e01086966ba537173
oxana1510 <oksi666@xaker.ru>
2019-03-29 13:14:29 +0300

    28.03

---
+++ scripts/script.js
@@ -0,0 +1,25 @@
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "flex";
+  evt.currentTarget.className += " active";
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+
+$(document).ready(function(){
+
+var url=document.location.href; $.each($(".languages ul li a"),function(){
+  if(this.href==url){$(this).addClass('active-button');};
+  });
+});
+
